6f27fefd92b92f3b8dbda2267b2a7ff2459706a0
Nicole <nfelch121394@gmail.com>
2015-02-04 18:20:02 -0500

    Android app

--- ToDoListApp/gen/com/example/todolistapp/R.java
+++ ToDoListApp/gen/com/example/todolistapp/R.java
@@ -24,18 +24,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
-        public static final int button1=0x7f080002;
-        public static final int editText1=0x7f080001;
-        public static final int list=0x7f080004;
-        public static final int tableRow1=0x7f080000;
-        public static final int tableRow2=0x7f080003;
-        public static final int task_description=0x7f080006;
-        public static final int task_id=0x7f080005;
+        public static final int action_settings=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int task_entry=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -43,7 +35,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int new_task=0x7f050002;
+        public static final int hello_world=0x7f050002;
     }
     public static final class style {
         /** 
