0344c038251300aa7a7d97acaa84813f865dd4d5
Nicole <nfelch121394@gmail.com>
2015-02-18 19:45:28 -0500

    Add create method to API controller

--- backend/app/controllers/api/v1/tasks_controller.rb
+++ backend/app/controllers/api/v1/tasks_controller.rb
@@ -4,4 +4,22 @@ class Api::V1::TasksController < ApplicationController
 
     render json: @tasks
   end
+
+  def create
+    @task = tasks.build(task_params)
+
+    if @task.save
+      render json: @task,
+        status: :created,
+        location: [:api, :v1, @task]
+    else
+      render json: { errors: @task.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).include(:name)
+  end
 end
